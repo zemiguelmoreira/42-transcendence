88967da147cebf381c8514eb29d23e4105cecc63
Temporary User <temp@example.com>
2024-05-20 00:56:38 +0100

    home

--- srcs/nginx/docker-entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# dir for self signed certificates
-mkdir -p /etc/nginx/ssl
-openssl req -x509 -nodes -days 365 -new -keyout /etc/nginx/ssl/transcendence.key -out /etc/nginx/ssl/transcendence.crt -subj "/CN=transcendence/O=42/OU=42Porto/C=PT/ST=Porto/L=Porto/emailAddress=transcendence@42.fr"
-# running nginx on foreground
-exec nginx -g "daemon off;"
