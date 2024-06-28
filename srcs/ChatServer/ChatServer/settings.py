ddd271db524515e32e93f59d09ebcd31e59cf4a2
jpalmeidaalves <jpalmeidaalves@hotmail.com>
2024-06-28 19:18:29 +0100

    chat fixed

--- srcs/ChatServer/ChatServer/settings.py
+++ srcs/ChatServer/ChatServer/settings.py
@@ -115,7 +115,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('redis', 6379)],
+            "hosts": [(os.getenv('REDIS_URL', 'redis://localhost:6379'))],
         },
     },
 }
