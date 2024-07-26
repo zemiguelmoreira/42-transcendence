fdbd6b85dbb9aa94aabde8a2b5fefbb8a26b718a
jdias-mo <jdias-mo@transcendence.myguest.virtualbox.org>
2024-07-26 17:41:33 +0100

    all chat, private chat, online users

--- srcs/ChatServer/chat/consumers.py
+++ srcs/ChatServer/chat/consumers.py
@@ -1,13 +1,23 @@
 import json
 import logging
+import os
+import django
+from django.conf import settings
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatServer.settings')
+django.setup()
+import asyncio
 from channels.generic.websocket import AsyncWebsocketConsumer
 from channels.db import database_sync_to_async
-from django.contrib.auth.models import AnonymousUser
-from django.conf import settings
-from rest_framework_simplejwt.exceptions import InvalidTokenError
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.exceptions import InvalidToken, TokenError
 from rest_framework_simplejwt.tokens import AccessToken
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+User = get_user_model()
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    online_users = set()
+
     async def connect(self):
         # Extract token from query string
         self.token = self.scope['query_string'].decode().split('=')[1]
@@ -28,45 +38,71 @@ class ChatConsumer(AsyncWebsocketConsumer):
             return
 
         # Set room and user group names
-        self.room_name = 'transcendence'
+        self.room_name = 'all'
         self.room_group_name = "chat_%s" % self.room_name
         self.user_group_name = "user_%s" % self.user.username
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
         await self.channel_layer.group_add(self.user_group_name, self.channel_name)
 
+        ChatConsumer.online_users.add(self.user.username)
+
         await self.accept()
+
         logging.info(f'User {self.user.username} connected to chat')
+        # Broadcast the updated list of online users
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'update.status',
+                'online_users': list(ChatConsumer.online_users)
+            }
+        )
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
         await self.channel_layer.group_discard(self.user_group_name, self.channel_name)
         logging.info(f'User {self.user.username} disconnected from chat')
 
+        ChatConsumer.online_users.discard(self.user.username)
+        # Broadcast the updated list of online users
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'update.status',
+                'online_users': list(ChatConsumer.online_users)
+            }
+        )
+
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
-        recipient = text_data_json.get("recipient")
-
+        recipient = text_data_json.get("recipient", None)
+        logging.info(f"Message: {message}, Recipient: {recipient}, Sender: {self.user.username}")
         if recipient:
+            if recipient == self.user.username:
+                return
             recipient_group_name = "user_%s" % recipient
             await self.channel_layer.group_send(
-                recipient_group_name, {"type": "private_message", "message": message, "sender": self.user.username}
+                recipient_group_name, {"type": "private.message", "message": message, "sender": self.user.username}
             )
         else:
             await self.channel_layer.group_send(
-                self.room_group_name, {"type": "chat_message", "message": message, "sender": self.user.username}
+                self.room_group_name, {"type": "chat.message", "message": message, "sender": self.user.username}
             )
 
     async def chat_message(self, event):
         message = event["message"]
+        sender = event["sender"]
 
-        await self.send(text_data=json.dumps({"message": message, "sender": event["sender"]}))
-
+        await self.send(text_data=json.dumps({"message": message, "sender": sender}))
+        logging.info(f"Message received: {message} from {sender}")
     async def private_message(self, event):
         message = event["message"]
+        sender = event["sender"]
 
-        await self.send(text_data=json.dumps({"message": message, "private": True, "sender": event["sender"]}))
+        await self.send(text_data=json.dumps({"message": message, "private": True, "sender": sender}))
+        logging.info(f"Private message received: {message} from {sender}")
 
     @database_sync_to_async
     def get_user_from_token(self, access_token):
@@ -76,3 +112,11 @@ class ChatConsumer(AsyncWebsocketConsumer):
            return user
         except (User.DoesNotExist):
             return None
+
+    async def update_status(self, event):
+        online_users = event['online_users']
+
+        await self.send(text_data=json.dumps({
+            'online_users': online_users
+        }))
+        logging.info(f"Online users updated: {online_users}")
--- srcs/frontend/www/chat.js
+++ srcs/frontend/www/chat.js
@@ -1,20 +1,83 @@
-const chat_accessToken = localStorage.getItem('accessToken');
-console.log('chat access token: ' + chat_accessToken);
-chat_socket = new WebSocket(`wss://${window.location.host}/chat/ws/?token=${chat_accessToken}`);
+let selectedUser = null;
 
-// Event listeners for WebSocket events
-chat_socket.addEventListener('open', () => {
-    console.log('WebSocket connection established');
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const chatLog = document.getElementById("chat-log");
+    const chatMessageInput = document.getElementById("chat-message-input");
+    const chatMessageSubmit = document.getElementById("chat-message-submit");
+    const onlineUsersList = document.getElementById("online-users-list");
 
-chat_socket.addEventListener('message', (event) => {
-    console.log('Received message:', event.data);
-});
+    const token = localStorage.getItem('accessToken');
 
-chat_socket.addEventListener('close', () => {
-    console.log('WebSocket connection closed');
-});
+    const chatSocket = new WebSocket(
+        `wss://${window.location.host}/chat/ws/?token=${token}`
+    );
 
-chat_socket.addEventListener('error', (error) => {
-    console.error('WebSocket error:', error);
+    chatSocket.onopen = function() {
+        console.log('WebSocket connection established');
+    };
+
+    chatSocket.onmessage = function(e) {
+        const data = JSON.parse(e.data);
+
+        if (data.message) {
+            const message = data.message;
+            const sender = data.sender;
+            const privateMessage = data.private ? "(Private)" : "";
+
+            const messageElement = document.createElement("div");
+            messageElement.textContent = `${privateMessage}${sender}: ${message}`;
+            chatLog.appendChild(messageElement);
+            chatLog.scrollTop = chatLog.scrollHeight;
+
+            console.log('Received message:', data);
+        } else if (data.online_users) {
+            onlineUsersList.innerHTML = '';
+            data.online_users.forEach(function(user) {
+                const userElement = document.createElement("div");
+                userElement.textContent = user;
+                userElement.classList.add("user-item");
+                userElement.onclick = function() {
+                    if (selectedUser === user) {
+                        selectedUser = null;
+                        userElement.classList.remove("selected");
+                    } else {
+                        selectedUser = user;
+                        document.querySelectorAll(".user-item").forEach(el => el.classList.remove("selected"));
+                        userElement.classList.add("selected");
+                    }
+                };
+                onlineUsersList.appendChild(userElement);
+            });
+            console.log('Online users:', data.online_users);
+        }
+    };
+
+    chatSocket.onclose = function(e) {
+        console.log('WebSocket connection closed');
+    };
+
+    chatSocket.onerror = function(error) {
+        console.error('WebSocket error:', error);
+    };
+
+    chatMessageSubmit.onclick = function() {
+        const message = chatMessageInput.value;
+        if (message) {
+            const messageData = {
+                "message": message
+            };
+            if (selectedUser) {
+                messageData.recipient = selectedUser;
+            }
+            chatSocket.send(JSON.stringify(messageData));
+            chatMessageInput.value = "";
+        }
+        console.log('Message sent:', message);
+    };
+
+    chatMessageInput.addEventListener("keyup", function(e) {
+        if (e.key === "Enter") {
+            chatMessageSubmit.click();
+        }
+    });
 });
